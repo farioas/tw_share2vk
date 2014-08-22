e1cf9f284255864fd09bcc0911b50c68ba1eded8
Sergey Zhuk <farioas@gmail.com>
2014-08-22 11:02:30 +0400

    urllib3 usage

--- tw_share2vk.py
+++ tw_share2vk.py
@@ -3,10 +3,11 @@
 #_author: Sergey Zhuk farioas@gmail.com
 # https://github.com/farioas/tw_share2vk #
 
-import urllib
+import urllib3
 import tweepy
 import vk_api
 import os.path
+urllib3.disable_warnings()
 
 # -------TWITTER_APP_CONFIG--------------------------------------------
 consumer_key = ""
@@ -73,7 +74,7 @@ def tw_auth():
 
 
 def vk_wall_text_photo(text, photo):
-    urllib.urlretrieve(photo, 'temp.jpg')
+    urllib3.urlretrieve(photo, 'temp.jpg')
     response = vk_api.VkUpload(vk_auth()).photo_wall('temp.jpg')[0]
     photo = 'photo{}_{}'.format(response['owner_id'], response['id'])
     vk_auth().method('wall.post', {'message': text, 'attachments': photo})
